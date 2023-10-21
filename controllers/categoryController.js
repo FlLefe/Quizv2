241d93d57ccd5e9ca63a5bb2d422e12f32dff715
Lefebvre <lefebvre.florian9@gmail.com>
2023-10-21 11:58:21 +0200

    Resolution du problème/ redirection fonctionnelle

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,6 +3,7 @@ const categories = require("../categories.json");
 exports.getCategory = (req, res, next) => {
     const categoryName = req.params.categoryName;
     const category = categories.find((element) => element.value === categoryName);
+    
     if(category){
         res.render("quizz", {
             category
--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routerCategories = express.Router();
+const categoryController = require("../controllers/categoryController");
+
 
 routerCategories.get("/categories", (req, res) => {
     res.render('categories');
 });
 
-routerCategories.get("/categories/:categoryName", routerCategories.getCategory);
+routerCategories.get("/categories/:categoryName", categoryController.getCategory);
 
 module.exports = routerCategories;
